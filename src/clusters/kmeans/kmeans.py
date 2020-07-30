bbd3c484eb0480c5523d01ec318d3ac9d8b9ed7d
Giovani Angelo <giovani.angelo@gmail.com>
2020-07-30 12:13:05 -0300

    feat: create Kmeans

---
+++ src/clusters/kmeans/kmeans.py
@@ -0,0 +1,35 @@
+from utils import distance
+import numpy as np
+import sys
+
+def get_labels(data, centroids):
+    labels = []
+    for d in data:
+        labels.append(
+            np.argmin([distance(d, centroid) for centroid in centroids])
+        )
+    return labels
+
+def kmeans(data, k):
+    centroids = []
+    centroids.append(data[np.random.randint(
+            data.shape[0]), :])
+
+    for c_id in range(k - 1):
+
+        dist = []
+        for i in range(data.shape[0]):
+            point = data[i, :]
+            d = sys.maxsize
+
+            for j in range(len(centroids)):
+                temp_dist = distance(point, centroids[j])
+                d = min(d, temp_dist)
+            dist.append(d)
+
+        dist = np.array(dist)
+        next_centroid = data[np.argmax(dist), :]
+        centroids.append(next_centroid)
+        dist = []
+    return centroids
+
---
+++ src/clusters/kmeans/utils.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def distance(p1, p2):
+    return np.sum((p1 - p2)**2)
