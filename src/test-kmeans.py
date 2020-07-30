1e7991de4ed6bc0c7cfbf807d3ab85921acbb594
Giovani Angelo <giovani.angelo@gmail.com>
2020-07-30 11:41:19 -0300

    refact: gitignore update

---
+++ src/test-kmeans.py
@@ -0,0 +1,45 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from kmeans import get_centroids, get_labels
+
+# function to plot the selected centroids
+def plot(data, centroids):
+    plt.scatter(data[:, 0], data[:, 1], marker = '.',
+                color = 'gray', label = 'data points')
+    plt.scatter(centroids[:-1, 0], centroids[:-1, 1],
+                color = 'black', label = 'previously selected centroids')
+    plt.scatter(centroids[-1, 0], centroids[-1, 1],
+                color = 'red', label = 'next centroid')
+    plt.title('Select % d th centroid'%(centroids.shape[0]))
+
+    plt.legend()
+    plt.xlim(-5, 12)
+    plt.ylim(-10, 15)
+    plt.show()
+
+
+mean_01 = np.array([0.0, 0.0])
+cov_01 = np.array([[1, 0.3], [0.3, 1]])
+dist_01 = np.random.multivariate_normal(mean_01, cov_01, 100)
+
+mean_02 = np.array([6.0, 7.0])
+cov_02 = np.array([[1.5, 0.3], [0.3, 1]])
+dist_02 = np.random.multivariate_normal(mean_02, cov_02, 100)
+
+mean_03 = np.array([7.0, -5.0])
+cov_03 = np.array([[1.2, 0.5], [0.5, 1,3]])
+dist_03 = np.random.multivariate_normal(mean_03, cov_01, 100)
+
+mean_04 = np.array([2.0, -7.0])
+cov_04 = np.array([[1.2, 0.5], [0.5, 1,3]])
+dist_04 = np.random.multivariate_normal(mean_04, cov_01, 100)
+
+data = np.vstack((dist_01, dist_02, dist_03, dist_04))
+np.random.shuffle(data)
+
+# call the initialize function to get the centroids
+centroids = get_centroids(data, k = 10)
+labels = get_labels(data, centroids)
+
+plt.scatter(data[:,0], data[:,1], c = labels)
+plt.show()
